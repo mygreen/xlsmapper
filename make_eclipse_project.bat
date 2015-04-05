402d315e429bc7a5288ef5dcd29df9ff99da92e2
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2015-04-05 12:36:22 +0900

    java7用の記述を追加。

--- make_eclipse_project.bat
+++ make_eclipse_project.bat
@@ -3,6 +3,11 @@
 %~d0
 cd %~p0
 
+if NOT "%JAVA_HOME_7%" == "" (
+    set JAVA_HOME="%JAVA_HOME_7%"
+)
+
+
 REM mvn clean
 mvn eclipse:eclipse -DdownloadSources=true -Declipse.useProjectReferences=false
 
