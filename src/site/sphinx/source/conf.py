db8604c2e1e1a0dd617aa93d94a582f56ffc0f14
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2015-06-29 03:14:58 +0900

    テーマをbizstyleに変更。

--- src/site/sphinx/source/conf.py
+++ src/site/sphinx/source/conf.py
@@ -112,7 +112,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -158,6 +159,9 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
+html_sidebars = {
+    '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'],
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
