8c38585f4cd6c26ef34e5fbf5c994679d0181d0e
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2015-07-16 02:11:09 +0900

    テスタのコミット

--- src/test/java/com/gh/mygreen/xlsmapper/UtilsTest.java
+++ src/test/java/com/gh/mygreen/xlsmapper/UtilsTest.java
@@ -1,39 +1,86 @@
 package com.gh.mygreen.xlsmapper;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 import java.awt.Point;
+import java.lang.reflect.Constructor;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.Vector;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.gh.mygreen.xlsmapper.Utils;
 
-
+/**
+ * {@link Utils}のテスタ
+ * 
+ * @since 0.5
+ * @author T.TSUCHIE
+ *
+ */
 public class UtilsTest {
     
     @Before
     public void setUp() throws Exception {
     }
     
+    /**
+     * {@link Utils#capitalize(String)}
+     */
     @Test
     public void testCapitalize() {
-        fail("Not yet implemented");
+        
+        assertThat(Utils.capitalize(null), is(nullValue()));
+        assertThat(Utils.capitalize(""), is(""));
+        assertThat(Utils.capitalize("cat"), is("Cat"));
+        assertThat(Utils.capitalize("cAt"), is("CAt"));
+        
     }
     
+    /**
+     * {@link Utils#uncapitalize(String)}
+     */
     @Test
     public void testUncapitalize() {
-        fail("Not yet implemented");
+        assertThat(Utils.uncapitalize(null), is(nullValue()));
+        assertThat(Utils.uncapitalize(""), is(""));
+        assertThat(Utils.uncapitalize("CAT"), is("cAT"));
+        assertThat(Utils.uncapitalize("Cat"), is("cat"));
     }
     
     @Test
     public void testIsEmpty() {
-        fail("Not yet implemented");
+        
+        // 文字列の場合
+        assertThat(Utils.isEmpty(""), is(true));
+        assertThat(Utils.isEmpty((String)null), is(true));
+        assertThat(Utils.isEmpty(" "), is(false));
+        assertThat(Utils.isEmpty("ab"), is(false));
+        assertThat(Utils.isEmpty(String.valueOf('\u0000')), is(true));
+        
     }
     
     @Test
     public void testIsNotEmpty() {
-        fail("Not yet implemented");
+        
+        // 文字列の場合
+        assertThat(Utils.isNotEmpty(""), is(false));
+        assertThat(Utils.isNotEmpty((String)null), is(false));
+        assertThat(Utils.isNotEmpty(" "), is(true));
+        assertThat(Utils.isNotEmpty("ab"), is(true));
+        assertThat(Utils.isNotEmpty(String.valueOf('\u0000')), is(false));
     }
     
     @Test
@@ -48,5 +95,90 @@ public class UtilsTest {
         
         Point p3 = Utils.parseCellAddress("a32A132");
         assertNull(p3);
+    }
+    
+    @Test
+    public void testConvertListToCollection() {
+        
+        final FactoryCallback<Class<?>, Object> beanFactory = new XlsMapperConfig().getBeanFactory();
+        
+        List<String> list = new ArrayList<>();
+        list.add("abc");
+        list.add("efg");
+        
+        Class toClass;
+        Collection<String> value;
+        
+        // 変換先がCollectionインタフェースの場合
+        toClass = Collection.class;
+        value = Utils.convertListToCollection(list, toClass, beanFactory);
+        assertThat(value, is(instanceOf(ArrayList.class)));
+        
+        // 変換先がListインタフェースの場合
+        toClass = List.class;
+        value = Utils.convertListToCollection(list, toClass, beanFactory);
+        assertThat(value, is(instanceOf(ArrayList.class)));
+        
+        // 変換先がArrayListで元のインスタンスと同じ場合。
+        toClass = ArrayList.class;
+        value = Utils.convertListToCollection(list, toClass, beanFactory);
+        assertThat(value, is(instanceOf(ArrayList.class)));
+        
+        // 変換先がVectorの場合
+        toClass = Vector.class;
+        value = Utils.convertListToCollection(list, toClass, beanFactory);
+        assertThat(value, is(instanceOf(Vector.class)));
+        
+        // 変換先がSetインタフェースの場合
+        toClass = Set.class;
+        value = Utils.convertListToCollection(list, toClass, beanFactory);
+        assertThat(value, is(instanceOf(LinkedHashSet.class)));
+        
+        // 変換先がQueueインタフェースの場合
+        toClass = Queue.class;
+        value = Utils.convertListToCollection(list, toClass, beanFactory);
+        assertThat(value, is(instanceOf(LinkedList.class)));
+        
+        // 変換先がDequeインタフェースの場合
+        toClass = Deque.class;
+        value = Utils.convertListToCollection(list, toClass, beanFactory);
+        assertThat(value, is(instanceOf(LinkedList.class)));
+        
+        // 変換先がLinkedListの場合
+        toClass = LinkedList.class;
+        value = Utils.convertListToCollection(list, toClass, beanFactory);
+        assertThat(value, is(instanceOf(LinkedList.class)));
+        
+        
+    }
+    
+    @Test
+    public void testConvertCollectionToList() {
+        
+        List<String> data = Arrays.asList("abc", "efg");
+        
+        Collection<String> collection;
+        List<String> value;
+        
+        // 変換元がリストの場合
+        collection = new ArrayList<>(data);
+        value = Utils.convertCollectionToList(collection);
+        assertThat(value, is(instanceOf(ArrayList.class)));
+        
+        // 変換元がSetの場合
+        collection = new LinkedHashSet<>(data);
+        value = Utils.convertCollectionToList(collection);
+        assertThat(value, is(instanceOf(ArrayList.class)));
+        
+        // 変換元がLinkedListの場合
+        collection = new LinkedList<>(data);
+        value = Utils.convertCollectionToList(collection);
+        assertThat(value, is(instanceOf(LinkedList.class)));
+        
+        // 変換元がArrayDequeの場合
+        collection = new ArrayDeque<>(data);
+        value = Utils.convertCollectionToList(collection);
+        assertThat(value, is(instanceOf(ArrayList.class)));
+        
     }
 }
