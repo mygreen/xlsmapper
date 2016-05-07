7b2b2a71d40bcde92c8c6933d93328a6a39981ca
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2016-05-07 12:54:55 +0900

    site 1.4.2

--- sphinx/_static/bizstyle.js
+++ sphinx/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("XlsMapper 1.4.1 ドキュメント");
+        $("li.nav-item-0 a").text("XlsMapper 1.4.2 ドキュメント");
     }
 });
