5bf816d2cbf887f6a6f268a68b366df2aeb87dd6
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2016-08-22 15:06:38 +0900

    Javadocの誤字の修正

--- src/main/java/com/gh/mygreen/xlsmapper/expression/ExpressionLanguage.java
+++ src/main/java/com/gh/mygreen/xlsmapper/expression/ExpressionLanguage.java
@@ -15,7 +15,7 @@ public interface ExpressionLanguage {
     /**
      * 引数で与えた式を評価する。
      * @param expression 評価対象の式。
-     * @param values 式中で利用可のな変数。
+     * @param values 式中で利用可な変数。
      * @return 評価した式。
      * @throws ExpressionEvaluationException 式のパースや評価に失敗した場合にスローされます。
      */
