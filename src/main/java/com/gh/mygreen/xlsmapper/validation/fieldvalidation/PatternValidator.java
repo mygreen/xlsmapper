1fc550e2aa0e4124465c9f93b2bf8d965003c4ff
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2016-10-30 15:33:45 +0900

    refs #85 PatternValidatorにパターン名を指定できるように修正。

--- src/main/java/com/gh/mygreen/xlsmapper/validation/fieldvalidation/PatternValidator.java
+++ src/main/java/com/gh/mygreen/xlsmapper/validation/fieldvalidation/PatternValidator.java
@@ -9,21 +9,57 @@ import com.gh.mygreen.xlsmapper.ArgUtils;
 /**
  * 正規表現を指定し、入力値チェックする。
  * <ul>
- *  <li>メッセージキーは、「cellFieldError.pattern」。
+ *  <li>メッセージキーは、「cellFieldError.pattern」。</li>
+ * </ul>
+ * 
+ * @version 1.5.1
  *
  */
 public class PatternValidator extends AbstractFieldValidator<String> {
     
     private final Pattern pattern;
     
+    private final String patternName;
+    
+    /**
+     * 正規表現のパターンを指定するコンストラクタ。
+     * @param pattern 正規表現のパターン。
+     */
     public PatternValidator(final String pattern) {
         this(Pattern.compile(pattern));
     }
     
+    /**
+     * 正規表現のパターンを指定するコンストラクタ。
+     * @param pattern 正規表現のパターン。
+     */
     public PatternValidator(final Pattern pattern) {
+        this(pattern, null);
+    }
+    
+    /**
+     * 正規表現のパターンとその名称を指定するコンストラクタ。
+     * 
+     * @since 1.5.1
+     * @param pattern 正規表現のパターン。
+     * @param patternName エラーメッセージ中で使用するパターンの名称。
+     */
+    public PatternValidator(final String pattern, final String patternName) {
+        this(Pattern.compile(pattern), patternName);
+    }
+    
+    /**
+     * 正規表現のパターンとその名称を指定するコンストラクタ。
+     * 
+     * @since 1.5.1
+     * @param pattern 正規表現のパターン。
+     * @param patternName エラーメッセージ中で使用するパターンの名称。
+     */
+    public PatternValidator(final Pattern pattern, final String patternName) {
         super();
         ArgUtils.notNull(pattern, "pattern");
         this.pattern = pattern;
+        this.patternName = patternName;
     }
     
     @Override
@@ -49,14 +85,23 @@ public class PatternValidator extends AbstractFieldValidator<String> {
         final LinkedHashMap<String, Object> vars = new LinkedHashMap<>();
         vars.put("validatedValue", value);
         vars.put("pattern", getPattern().pattern());
+        vars.put("patternName", getPatternName());
         return vars;
     }
     
     /**
-     * pattern を取得する
-     * @return the pattern
+     * 
+     * @return pattern を取得する
      */
     public Pattern getPattern() {
         return pattern;
+    }
+    
+    /**
+     * @since 1.5.1
+     * @return パターン名を取得する。指定されていない場合はnullを返す。
+     */
+    public String getPatternName() {
+        return patternName;
     }
 }
