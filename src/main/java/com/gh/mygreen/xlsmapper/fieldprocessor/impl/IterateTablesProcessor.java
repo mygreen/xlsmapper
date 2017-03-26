06a1eade92c39251d35ed5c204f2d0d23d879763
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2017-03-26 11:23:09 +0900

    IterateTablesにVerticalを対応

--- src/main/java/com/gh/mygreen/xlsmapper/fieldprocessor/impl/IterateTablesProcessor.java
+++ src/main/java/com/gh/mygreen/xlsmapper/fieldprocessor/impl/IterateTablesProcessor.java
@@ -22,6 +22,8 @@ import com.gh.mygreen.xlsmapper.annotation.XlsHorizontalRecordsForIterateTables;
 import com.gh.mygreen.xlsmapper.annotation.XlsIterateTables;
 import com.gh.mygreen.xlsmapper.annotation.XlsLabelledCell;
 import com.gh.mygreen.xlsmapper.annotation.XlsLabelledCellForIterateTable;
+import com.gh.mygreen.xlsmapper.annotation.XlsVerticalRecords;
+import com.gh.mygreen.xlsmapper.annotation.XlsVerticalRecordsForIterateTables;
 import com.gh.mygreen.xlsmapper.fieldaccessor.FieldAccessor;
 import com.gh.mygreen.xlsmapper.fieldaccessor.FieldAccessorProxy;
 import com.gh.mygreen.xlsmapper.fieldaccessor.FieldAccessorProxyComparator;
@@ -34,6 +36,7 @@ import com.gh.mygreen.xlsmapper.util.FieldAccessorUtils;
 import com.gh.mygreen.xlsmapper.util.POIUtils;
 import com.gh.mygreen.xlsmapper.util.Utils;
 import com.gh.mygreen.xlsmapper.validation.MessageBuilder;
+import com.gh.mygreen.xlsmapper.xml.AnnotationReader;
 
 
 /**
@@ -95,8 +98,11 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
         
     }
     
-    protected List<?> loadTables(final Sheet sheet, final XlsIterateTables iterateTablesAnno, final FieldAccessor accessor,
+    private List<?> loadTables(final Sheet sheet, final XlsIterateTables iterateTablesAnno, final FieldAccessor accessor,
             final Class<?> tableClass, final XlsMapperConfig config, final LoadingWorkObject work) throws XlsMapperException {
+        
+        // アノテーションの整合性のチェック
+        checkRecordAnnotation(tableClass, work.getAnnoReader());
         
         final List<Object> resultTableList = new ArrayList<>();
         
@@ -136,8 +142,7 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
             loadMultipleHorizontalTableCell(sheet, tableObj, currentCell, iterateTablesAnno, config, work);
             
             // process vertial table
-            //TODO: verticalの場合は、走査方向が違うのでできないので、例外をスローすべき。
-//            loadMultipleVerticalTableCell(sheet, tableObj, currentCell, iterateTablesAnno, config, setting);
+            loadMultipleVerticalTableCell(sheet, tableObj, currentCell, iterateTablesAnno, config, work);
             
             resultTableList.add(tableObj);
             after = currentCell;
@@ -166,6 +171,30 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
     }
     
     /**
+     * レコード用のアノテーションの整合性のチェックを行う。
+     * <p>{@link XlsHorizontalRecords}と{@link XlsVerticalRecords}は、どちらか一方のみ指定可能。</p>
+     * @param tableClass テーブル用のクラス情報
+     * @param annoReader アノテーションの提供クラス
+     */
+    private void checkRecordAnnotation(final Class<?> tableClass, final AnnotationReader annoReader) {
+        
+        final int horizontalSize = FieldAccessorUtils.getPropertiesWithAnnotation(
+                tableClass, annoReader, XlsHorizontalRecords.class)
+                .size();
+        
+        final int verticalSize = FieldAccessorUtils.getPropertiesWithAnnotation(
+                tableClass, annoReader, XlsVerticalRecords.class)
+                .size();
+        
+        if(horizontalSize > 0 && verticalSize > 0) {
+            throw new AnnotationInvalidException(MessageBuilder.create("anno.XlsIterateTables.horizontalAndVertical")
+                    .varWithClass("tableClass", tableClass)
+                    .format());
+        }
+        
+    }
+    
+    /**
      * XlsLabelledCellによる繰り返しを処理する。
      * @param sheet
      * @param tableObj
@@ -173,7 +202,7 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
      * @param config
      * @throws XlsMapperException
      */
-    protected void loadSingleLabelledCell(final Sheet sheet, final Object tableObj, 
+    private void loadSingleLabelledCell(final Sheet sheet, final Object tableObj, 
             final Cell headerCell, final XlsMapperConfig config, final LoadingWorkObject work) throws XlsMapperException {
         
         final LabelledCellProcessor labelledCellProcessor = 
@@ -211,7 +240,7 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
         }
     }
     
-    protected void loadMultipleHorizontalTableCell(final Sheet sheet, final Object tableObj, 
+    private void loadMultipleHorizontalTableCell(final Sheet sheet, final Object tableObj, 
             final Cell headerCell, final XlsIterateTables iterateTablesAnno,
             final XlsMapperConfig config, final LoadingWorkObject work) throws XlsMapperException {
         
@@ -243,7 +272,7 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
             }
         }
         
-        // 順番を並び替えて保存処理を実行する
+        // 順番を並び替えて読み込み処理を実行する
         Collections.sort(accessorProxies, new FieldAccessorProxyComparator());
         for(FieldAccessorProxy adaptorProxy : accessorProxies) {
             adaptorProxy.loadProcess(sheet, tableObj, config, work);
@@ -251,35 +280,45 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
         
     }
     
-//    protected void loadMultipleVerticalTableCell(final Sheet sheet, final Object tableObj, 
-//            final Cell headerCell, final XlsIterateTables iterateTables,
-//            final XlsMapperConfig config, LoadingProcessSetting setting) throws XlsMapperException {
-//        
-//        int headerColumn = headerCell.getColumnIndex();
-//        int headerRow = headerCell.getRowIndex();
-//        
-//        if (iterateTables.bottom() > 0) {
-//            // if positive value set to bottom(), column index of table header move
-//            headerColumn += iterateTables.bottom();
-//        }
-//        
-//        final VerticalRecordsProcessor processor = new VerticalRecordsProcessor();
-//        final List<FieldAdaptor> properties = Utils.getLoadingPropertiesWithAnnotation(
-//                tableObj.getClass(), setting.getAnnoReader(), XlsVerticalRecords.class);
-//        
-//        for(FieldAdaptor property : properties) {
-//            final XlsVerticalRecords ann = property.getLoadingAnnotation(XlsVerticalRecords.class);
-//            
-//            if(iterateTables.tableLabel().equals(ann.tableLabel())) {
-//                
-//                final XlsVerticalRecords records = new XlsVerticalRecordsForIterateTables(ann, headerColumn, headerRow);
-//                
-//                // vertical record-mapping
-//                processor.loadProcess(sheet, tableObj, records, property, config, setting);
-//            }
-//        }
-//        
-//    }
+    private void loadMultipleVerticalTableCell(final Sheet sheet, final Object tableObj, 
+            final Cell headerCell, final XlsIterateTables iterateTablesAnno,
+            final XlsMapperConfig config, final LoadingWorkObject work) throws XlsMapperException {
+        
+        int headerColumn = headerCell.getColumnIndex();
+        int headerRow = headerCell.getRowIndex();
+        
+        if (iterateTablesAnno.bottom() > 0) {
+            // if positive value set to bottom(), row index of table header move
+            headerRow += iterateTablesAnno.bottom();
+        }
+        
+        final VerticalRecordsProcessor processor = 
+                (VerticalRecordsProcessor) config.getFieldProcessorRegistry().getLoadingProcessor(XlsVerticalRecords.class);
+        
+        final List<FieldAccessor> properties = FieldAccessorUtils.getPropertiesWithAnnotation(
+                tableObj.getClass(), work.getAnnoReader(), XlsVerticalRecords.class)
+                .stream()
+                .filter(p -> p.isReadable())
+                .collect(Collectors.toList());
+        
+        final List<FieldAccessorProxy> accessorProxies = new ArrayList<>();
+        for(FieldAccessor property : properties) {
+            final XlsVerticalRecords anno = property.getAnnotation(XlsVerticalRecords.class).get();
+            
+            if(iterateTablesAnno.tableLabel().equals(anno.tableLabel())) {
+                
+                final XlsVerticalRecords recordsAnno = new XlsVerticalRecordsForIterateTables(anno, headerColumn, headerRow);
+                accessorProxies.add(new FieldAccessorProxy(recordsAnno, processor, property));
+            }
+        }
+        
+        // 順番を並び替えて読み込み処理を実行する
+        Collections.sort(accessorProxies, new FieldAccessorProxyComparator());
+        for(FieldAccessorProxy adaptorProxy : accessorProxies) {
+            adaptorProxy.loadProcess(sheet, tableObj, config, work);
+        }
+        
+    }
     
     @Override
     public void saveProcess(final Sheet sheet, final Object beansObj, final XlsIterateTables anno, final FieldAccessor accessor,
@@ -319,9 +358,12 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
                     }
     }
     
-    protected void saveTables(final Sheet sheet, final XlsIterateTables iterateTablesAnno, final FieldAccessor accessor,
+    private void saveTables(final Sheet sheet, final XlsIterateTables iterateTablesAnno, final FieldAccessor accessor,
             final Class<?> tableClass, final List<Object> resultTableList,
             final XlsMapperConfig config, final SavingWorkObject work) throws XlsMapperException {
+        
+        // アノテーションの整合性のチェック
+        checkRecordAnnotation(tableClass, work.getAnnoReader());
         
         Cell after = null;
         Cell currentCell = null;
@@ -370,8 +412,8 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
             // process horizontal table.
             saveMultipleHorizontalTableCell(sheet, tableObj, currentCell, iterateTablesAnno, config, work);
             
-            // TODO process vertical table
-//            saveMultipleVerticalTableCell(sheet, tableObj, currentCell, iterateTablesAnno, config, setting);
+            // process vertical table
+            saveMultipleVerticalTableCell(sheet, tableObj, currentCell, iterateTablesAnno, config, work);
             
             after = currentCell;
             
@@ -392,7 +434,7 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
         
     }
     
-    protected void saveSingleLabelledCell(final Sheet sheet, final Object tableObj, final Cell headerCell,
+    private void saveSingleLabelledCell(final Sheet sheet, final Object tableObj, final Cell headerCell,
             final XlsMapperConfig config, final SavingWorkObject work) throws XlsMapperException {
         
         final LabelledCellProcessor labelledCellProcessor = 
@@ -432,7 +474,7 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
         
     }
     
-    protected void saveMultipleHorizontalTableCell(final Sheet sheet, final Object tableObj, final Cell headerCell,
+    private void saveMultipleHorizontalTableCell(final Sheet sheet, final Object tableObj, final Cell headerCell,
             final XlsIterateTables iterateTables, final XlsMapperConfig config, final SavingWorkObject work) throws XlsMapperException {
         
         final int headerColumn = headerCell.getColumnIndex();
@@ -443,7 +485,8 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
             headerRow += iterateTables.bottom();
         }
         
-        final HorizontalRecordsProcessor processor = (HorizontalRecordsProcessor) config.getFieldProcessorRegistry().getSavingProcessor(XlsHorizontalRecords.class);
+        final HorizontalRecordsProcessor processor = 
+                (HorizontalRecordsProcessor) config.getFieldProcessorRegistry().getSavingProcessor(XlsHorizontalRecords.class);
         
         final List<FieldAccessor> properties = FieldAccessorUtils.getPropertiesWithAnnotation(
                 tableObj.getClass(), work.getAnnoReader(), XlsHorizontalRecords.class)
@@ -473,33 +516,46 @@ public class IterateTablesProcessor extends AbstractFieldProcessor<XlsIterateTab
         
     }
     
-//    protected void saveMultipleVerticalTableCell(final Sheet sheet, final Object tableObj, final Cell headerCell,
-//            final XlsIterateTables iterateTables, final XlsMapperConfig config, final SavingProcessSetting setting) throws XlsMapperException {
-//        
-//        int headerColumn = headerCell.getColumnIndex();
-//        final int headerRow = headerCell.getRowIndex();
-//        
-//        if (iterateTables.bottom() > 0) {
-//            // if positive value set to bottom(), row index of table header move
-//            headerColumn += iterateTables.bottom();
-//        }
-//        
-//        final VerticalRecordsProcessor processor = new VerticalRecordsProcessor();
-//        final List<FieldAdaptor> properties = Utils.getSavingPropertiesWithAnnotation(
-//                tableObj.getClass(), setting.getAnnoReader(), XlsVerticalRecords.class);
-//        
-//        for(FieldAdaptor property : properties) {
-//            
-//            final XlsVerticalRecords anno = property.getSavingAnnotation(XlsVerticalRecords.class);
-//            
-//            if (iterateTables.tableLabel().equals(anno.tableLabel())) {
-//                // 処理対象と同じテーブルラベルのとき、マッピングを実行する。
-//                final XlsVerticalRecords recordsAnno = new XlsVerticalRecordsForIterateTables(anno, headerColumn, headerRow);
-//                processor.saveProcess(sheet, tableObj, recordsAnno, property, config, setting);
-//            }
-//            
-//        }
-//        
-//    }
+    private void saveMultipleVerticalTableCell(final Sheet sheet, final Object tableObj, final Cell headerCell,
+            final XlsIterateTables iterateTablesAnno, final XlsMapperConfig config, final SavingWorkObject work) throws XlsMapperException {
+        
+        int headerColumn = headerCell.getColumnIndex();
+        int headerRow = headerCell.getRowIndex();
+        
+        if (iterateTablesAnno.bottom() > 0) {
+            // if positive value set to bottom(), row index of table header move
+            headerRow += iterateTablesAnno.bottom();
+        }
+        
+        final VerticalRecordsProcessor processor = 
+                (VerticalRecordsProcessor) config.getFieldProcessorRegistry().getSavingProcessor(XlsVerticalRecords.class);
+        
+        final List<FieldAccessor> properties = FieldAccessorUtils.getPropertiesWithAnnotation(
+                tableObj.getClass(), work.getAnnoReader(), XlsVerticalRecords.class)
+                .stream()
+                .filter(p -> p.isWritable())
+                .collect(Collectors.toList());
+        
+        final List<FieldAccessorProxy> accessorProxies = new ArrayList<>();
+        for(FieldAccessor property : properties) {
+            
+            final XlsVerticalRecords anno = property.getAnnotation(XlsVerticalRecords.class).get();
+            
+            // 処理対象と同じテーブルラベルのとき、マッピングを実行する。
+            if(iterateTablesAnno.tableLabel().equals(anno.tableLabel())) {
+                final XlsVerticalRecords recordsAnno = new XlsVerticalRecordsForIterateTables(anno, headerColumn, headerRow);
+                accessorProxies.add(new FieldAccessorProxy(recordsAnno, processor, property));
+                
+            }
+            
+        }
+        
+        // 順番を並び替えて保存処理を実行する
+        Collections.sort(accessorProxies, new FieldAccessorProxyComparator());
+        for(FieldAccessorProxy adaptorProxy : accessorProxies) {
+            adaptorProxy.saveProcess(sheet, tableObj, config, work);
+        }
+        
+    }
     
 }
